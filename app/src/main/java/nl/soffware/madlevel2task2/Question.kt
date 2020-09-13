a52c8e781785b79bc77864dd58aef77fb3948a02
David Soff <david@soff.nl>
2020-09-13 22:34:01 +0200

    initial commit

---
+++ app/src/main/java/nl/soffware/madlevel2task2/Question.kt
@@ -0,0 +1,21 @@
+package nl.soffware.madlevel2task2
+
+class Question(
+    var question: String,
+    var answer: Boolean
+) {
+    companion object {
+        val QUESTIONS = arrayOf(
+            "A 'val' and 'var' are the same,",
+            "Mobile Application Development grants 12 EC.",
+            "A Unit in Kotlin corresponds to a void in Java.",
+            "In Kotlin ' When' replaces the 'switch' operator in Java."
+        )
+        val answers = arrayOf(
+            false,
+            false,
+            true,
+            true
+        )
+    }
+}
